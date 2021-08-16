a18f77b594de98e455e4cf87034345e229b293a4
bogoevski-s <slave.bogoevski@yahoo.com>
2021-08-16 10:58:18 +0200

    homework_1

---
+++ SEDC.PizzaApp/SEDC.PizzaApp.Web/Controllers/PizzaController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SEDC.PizzaApp.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SEDC.PizzaApp.Web.Controllers
+{
+    public class PizzaController : Controller
+    {
+        public IActionResult Index()
+        {
+            var model = StaticDB.Pizzas;
+            return new JsonResult(model);
+        }
+        [HttpGet]
+        public IActionResult Details(int id)
+        {
+            var model = StaticDB.Pizzas.FirstOrDefault(x => x.Id == id);
+            if(model == null)
+            {
+                return RedirectToAction("Error", "Home");
+            }
+            return View(model);
+        }
+    }
+}
